e9fd4484b992a433f355a14a146c8b1a31a5607b
Dianeod <dodonoghue@kx.com>
2020-09-14 12:05:34 +0100

    installed extra requirements for tests

--- build/test.bat
+++ build/test.bat
@@ -2,6 +2,9 @@ if defined QLIC_KC (
         pip -q install -r requirements.txt
 	echo getting test.q from embedpy
         git clone https://github.com/KxSystems/ml.git
+	git clone https://github.com/KxSystems/nlp.git
+	pip install gensim
+	pip install sobol-seq
         curl -fsSL -o test.q https://github.com/KxSystems/embedpy/raw/master/test.q
         q test.q -q
 )
